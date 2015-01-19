a62c1cbc0bffe0e1ee255835a2251f7061d7d8ec
jsm94 <smjuan24@gmail.com>
2015-01-19 14:35:04 +0100

    Cambios generales

---
+++ bloques/head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Gestiona tus archivos en la nube">
+    <meta name="author" content="Juan Antonio Seco Merchán">
+    <title>phpBox</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/material.min.css">
+    <link rel="stylesheet" href="css/ripples.min.css">
+    <link rel="stylesheet" href="css/material-wfont.min.css">
+    <style>body{padding-top:50px;}.starter-template{padding:40px 15px;text-align:center;}</style>
+
+    <!--[if IE]>
+        <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
---
+++ global.php
@@ -0,0 +1,5 @@
+<?php
+// Este archivo contendrá todas las variables globales de la aplicación
+$_URL = 'phpBox';
+$BOX_URI = $_SERVER['DOCUMENT_ROOT'] . '/' . $_URL . '/';
+$BOX_BLOCKS = $BOX_URI . 'bloques/';
